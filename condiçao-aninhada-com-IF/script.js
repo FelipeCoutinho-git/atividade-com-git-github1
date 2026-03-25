dda3744de190fbb6e38610dd7d7c17605024f8ff
FelipeCoutinho-git <coutinho.felipem@gmail.com>
2026-03-25 10:17:03 -0300

    Atividade aninhada

---
+++ condiçao-aninhada-com-IF/script.js
@@ -0,0 +1,17 @@
+let renda = Number(prompt("Digite sua renda: "));
+
+// ! simbolo de negação 
+// isNaN: is not number (verifica se NÃO é um numero)
+
+if(!isNaN(renda)){
+if (renda >= 3000) {
+        let nomeLimpo = prompt("Nome limpo? (sim ou nao)");
+        if (nomeLimpo === "sim") {
+                alert("Financiamento aprovado");
+        } else {
+                alert("Nome negativo");
+        }
+        } else {
+        alert("Renda insuficiente");
+        }
+}
